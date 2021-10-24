a02ab4c559148f1d10b61b85e14eeb37eb7f1407
VgionTech <(0$t\U9i9b.{3k*>
2021-10-24 16:52:42 +0530

    javascript enabled. responsive. back button. zoom controls. webview clients.

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.webview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +20,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webView);
+
+        setupWebView();
+    }
+
+    private void setupWebView() {
+        // Configure related browser settings
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        // Configure the client to use when opening URLs
+        webView.setWebViewClient(new WebViewClient() {
+            ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressDialog.setTitle("Loading...");
+                progressDialog.setMessage("Please wait...");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+            }
+
+            @Override
+            public void onPageCommitVisible(WebView view, String url) {
+                super.onPageCommitVisible(view, url);
+                if (progressDialog != null){
+                    progressDialog.dismiss();
+                }
+            }
+
+        });
+
+        // Enable responsive layout
+        webView.getSettings().setUseWideViewPort(true);
+
+        // Zoom out if the content width is greater than the width of the viewport
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true); // allow pinch to zooom
+        webView.getSettings().setDisplayZoomControls(true); // disable the default zoom controls on the page
+
         webView.loadUrl("https://www.google.com/");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
